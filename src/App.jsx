374a50ffc947a1f7ff32c6946fcdf83499500580
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-05-13 20:32:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Jhinku React is a React Boilerplate
+
+
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
